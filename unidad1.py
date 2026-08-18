21ddf14f15f05ce815dee28a893abe6383c0a92e
Juan Rosas <160557063+Juannnrv@users.noreply.github.com>
2026-08-18 17:04:13 -0500

    Ejercicio 1: Convertir minutos a horas y minutos

---
+++ unidad1.py
@@ -0,0 +1,8 @@
+
+# Ejercicio 1: Convertir minutos a horas y minutos
+time = int(input("Ingrese los minutos: "))
+
+horas = time // 60
+minutos = time % 60
+
+print(horas, "horas y", minutos, "minutos")
