329005e7a517b961337a1fedcbea2a4298db82a5
Juan Rosas <160557063+Juannnrv@users.noreply.github.com>
2026-08-18 17:25:21 -0500

    Ejercicio 4: Contar cuántas personas son mayores de edad

--- unidad1.py
+++ unidad1.py
@@ -1,12 +1,10 @@
 
-# Ejercicio 3: Clasificar una nota
-note= float(input("Ingrese la nota: "))
+# Ejercicio 4: Contar cuántas personas son mayores de edad
+edades = [23, 12, 48, 15, 11, 10]
 
-if note >= 4.0:
-    print("Alta")
+count = 0
+for edad in edades:
+    if edad >= 18:
+        count += 1
 
-elif note >= 3.0 and note < 3.9:
-    print("Media")
-
-else:
-    print("Baja")
+print("Número de personas mayores de edad:", count)
