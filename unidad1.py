732ce64a029b2f72a19f706e14dd70a05c69b626
Juan Rosas <160557063+Juannnrv@users.noreply.github.com>
2026-08-18 17:51:11 -0500

    Ejercicio 8: Sensor de medición de anomalias

--- unidad1.py
+++ unidad1.py
@@ -1,10 +1,8 @@
 
-# Ejercicio 7: Calcular el área y el perímetro de un rectángulo
-base = float(input("Ingrese la base de su rectángulo: "))
-altura = float(input("Ingrese la altura de su rectángulo: "))
+# Ejercicio 8: Sensor de medición de anomalias
+medicion = int(input("Ingrese la medición: "))
 
-area = base * altura
-perimetro = 2 * (base + altura)
-
-print("El área del rectángulo es:", area)
-print("El perímetro del rectángulo es:", perimetro)
+if medicion >= 40:
+    print("El sensor detecto una ANOMALIA", medicion)
+else:
+    print("El sensor considera la medición normal", medicion)
