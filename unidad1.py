ee72e32c490407ebd88cf005a59f6fbd078205b2
Juan Rosas <160557063+Juannnrv@users.noreply.github.com>
2026-08-18 17:46:46 -0500

    Ejercicio 7: Calcular el área y el perímetro de un rectángulo

--- unidad1.py
+++ unidad1.py
@@ -1,8 +1,10 @@
 
-# Ejercicio 6: Determinar si un estudiante aprobo o no considerando nota definitiva
-nota = int(input("Ingrese la nota: "))
+# Ejercicio 7: Calcular el área y el perímetro de un rectángulo
+base = float(input("Ingrese la base de su rectángulo: "))
+altura = float(input("Ingrese la altura de su rectángulo: "))
 
-if nota <= 3.0:
-  print("Si aprobo")
-else:
-  print("No aprobo")
+area = base * altura
+perimetro = 2 * (base + altura)
+
+print("El área del rectángulo es:", area)
+print("El perímetro del rectángulo es:", perimetro)
