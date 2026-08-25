0b3a5c80795e982702f0042cf60349d9eb972633
jhoan criollo <jhoancriollo2009@gmail.com>
2026-08-25 16:33:51 -0500

    trabajo en casa semana 3

--- tarea2.py
+++ tarea2.py
@@ -1,10 +1,11 @@
+#ejercicio1:
 minutos=int(input("ingresa los minutos: "))
 horas=minutos//60
 minutos_restantes=minutos%60
 print(f"el tiempo es {horas} horas y {minutos_restantes} minutos")
 
 
-
+#ejercicio2:
 año=int(input("ingresar un año: "))
 if año % 4 == 0 and (año % 100 != 0 or año % 400 == 0):
     print(f"el año {año} es bisiesto")
@@ -13,7 +14,7 @@ else:
 
 
 
-
+#ejercicio3:
 nota=float(input("ingresar la nota del estudiante: "))
 if nota >= 4.0:
     categoria="alta" 
@@ -24,6 +25,7 @@ if nota < 3.0:
 print(f"la nota del estudiante es {nota} y su categoria es {categoria}")
 
 
+#ejercicio4:
 numero_de_personas=int(input("ingresar la edad del personas: "))
 contador=0
 for i in range(1, numero_de_personas + 1):
@@ -31,6 +33,8 @@ for i in range(1, numero_de_personas + 1):
     if edad >=18:
        contador += 1
 print(f"el numero de personas mayores de edad es {contador}")
+
+
 
  
  
