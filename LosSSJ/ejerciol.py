5b4a23a3dee622bb18f0dd5cf8d664db6396b30f
jhoan criollo <jhoancriollo2009@gmail.com>
2026-08-24 22:18:37 -0500

    Corrigiendo código de ejerciol.py - ejercicios en clases

--- LosSSJ/ejerciol.py
+++ LosSSJ/ejerciol.py
@@ -1,2 +1,40 @@
-01261151007
-hag
+numero=int(input("ingresar un numero entero: "))
+if numero % 2 ==0:
+    print("el numero {numero} es par")
+else:
+    print("el numero {numero} es impar")
+
+
+nota=float(input("ingresar la nota del estudiante: "))
+if nota >= 3.0 :
+    print(f"el estudiante aprobo con nota {nota}")
+else:
+    print(f"el estudiante reprobo con nota {nota}")
+
+
+
+base=float(input("ingresar la base del triangulo: "))
+altura=float(input("ingresar la altura del triangulo: "))
+area=(base*altura)/2
+print(f"el area del triangulo es {area}")
+lado1=float(input("ingresar el primer lado del triangulo: "))
+lado2=float(input("ingresar el segundo lado del triangulo: "))
+lado3=float(input("ingresar el tercer lado del triangulo: "))
+perimetro=lado1+lado2+lado3
+print(f"el perimetro del triangulo es {perimetro}")
+
+
+
+
+
+medicion=float(input("ingresar la medicion del sensor"))
+if medicion>= 40:
+   print("se considera anomalia")
+else:
+   print("es normal")    
+
+
+
+
+
+
